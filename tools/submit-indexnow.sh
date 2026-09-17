0c4afc17beaf3f2fb83ecf1647610777f4538a38
Serm17 <okayssr77@gmail.com>
2026-09-17 22:12:10 +0900

    Fix echo statement formatting in submit-indexnow.sh

--- tools/submit-indexnow.sh
+++ tools/submit-indexnow.sh
@@ -5,4 +5,3 @@ curl -sS -X POST 'https://api.indexnow.org/indexnow' \
   -d '{"host":"sonserim.me.kr","key":"6ff0e06ba7aceee660b6f06c3593a11f","keyLocation":"https://sonserim.me.kr/6ff0e06ba7aceee660b6f06c3593a11f.txt","urlList":["https://sonserim.me.kr/","https://sonserim.me.kr/about.html","https://sonserim.me.kr/good-geo.html","https://sonserim.me.kr/geo-measurement.html","https://sonserim.me.kr/project-01.html","https://sonserim.me.kr/project-02.html"]}'
 echo
 echo "IndexNow submitted. This notifies participating engines; it does not guarantee indexing."
-
